208ae5858e1c69db632d07aa0d436684f80a6e11
Janet Chavez Erazo <76828004+erazo-janet@users.noreply.github.com>
2021-01-14 07:23:53 -0800

    Hello World in C++

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+//#!/bin/C++
+//incllude tells the compiler to include the standard iostream file which contains declarations of all the standard input/output library functions
+#include<iostream>
+
+using namespace std;
+
+//Execute the program
+int main() {
+    cout << "Hello World!"; 
+    cout << "I'm a C++ Program.";
+    return 0; //this terminates the main() function and return 0
+}
